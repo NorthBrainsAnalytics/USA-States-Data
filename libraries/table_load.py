f1a459fbe0029c8df1857eb58a2c534c659cf51c
NorthBrains Analytics <nbanalitycs@gmail.com>
2022-10-01 17:28:53 +0200

    Second initial commit
    01.10.22 r.
    
    1) made BEALineCodePick.py script to checking line codes
    2) builded table_load.py (class/method) to load chosen BEA's frame
    3) started USA-by-State.ipynb

---
+++ libraries/table_load.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import requests as req
+
+
+class GetRegionalData:
+    """ Upload BEA's data by line code and table name - assigned to Regional Dataset"""
+
+    def __init__(self):
+        self.params_x = {'USERID': '64C1A606-C86B-4702-A5D0-977F79A936D1',
+                  'METHOD': 'GETDATA',
+                  'DATASETNAME': 'REGIONAL',
+                  'YEAR': 'LAST10',
+                  'TABLENAME': '',
+                  'GEOFIPS': 'STATE',
+                  'LINECODE': 1,
+                  'RESULTFORMAT': 'JSON'}
+
+    def bea_frame_return(self, line_code, table_name) -> dict:
+        """
+        🧠 Arguments:
+        ----
+        line_code = parameter should contain integer as a line code key.
+        example "line_code = 1" ✅
+        To check available line codes launch 'BEALineCodePick.py' in your terminal 💻
+
+        table_name = parameter should contain string as a chosen table name.
+        example "table_name = 'SAEMP25N'" ✅
+        To check available tables see BEA Doc 📒
+
+        """
+        url = 'https://apps.bea.gov/api/data'
+        self.params_x.update({'TABLENAME': table_name, 'LINECODE': line_code})
+        get = req.get(url, self.params_x).json()['BEAAPI']['Results']['Data']
+
+        return pd.DataFrame(get)
+
+
+# x = GetRegionalData()
+# data = x.bea_frame_return(line_code=10, table_name='SAEMP25N')
