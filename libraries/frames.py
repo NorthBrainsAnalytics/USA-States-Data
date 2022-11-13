aea116c886ac81b219a23c8f7a56b8906df97f18
NorthBrains Analytics <nbanalitycs@gmail.com>
2022-11-13 21:02:52 +0100

    Commit 7
    13.11.2022
    
    1. Added code in frames.py to call the column percentage_change_by_previous_year

--- libraries/frames.py
+++ libraries/frames.py
@@ -11,8 +11,7 @@ consumer_index = pd.read_excel(
     "~/Documents/USA-States-Data/libraries/data/consumer_sentiment_index.xlsx").rename \
     (columns={'DATE OF SURVEY': 'month_of_survey',
               'Unnamed: 1': 'year',
-              'INDEX OF CONSUMER SENTIMENT': 'consumer_sentiment_index'},
-     inplace=False)
+              'INDEX OF CONSUMER SENTIMENT': 'consumer_sentiment_index'}, inplace=False)
 
 
 class MeanFrames:
@@ -43,6 +42,7 @@ class MeanFrames:
         else:
             print('Wrong table name or year 🙀')
 
+
     def frames_total(self, frame, range1: int, range2: int) -> dict:
         """ Printing entire table with indicated range of years """
         if frame == 'inflation_rate' and range1 in range(1998, 2023) and range2 != range1:
@@ -56,8 +56,32 @@ class MeanFrames:
             mean_total2 = (consumer_index[['year', 'consumer_sentiment_index']]
                            .groupby('year').agg('mean').round(decimals=2))
             mean_total2.reset_index(inplace=True)
-            df2 = mean_total2[(range1 <= mean_total2.year) & (mean_total2.year <= range2)].reset_index(drop=True)
-            return df2
+            df = mean_total2[(range1 <= mean_total2.year) & (mean_total2.year <= range2)].reset_index(drop=True)
+
+            # adding column with percentage differences between years
+            years = (consumer_index[['year', 'consumer_sentiment_index']]
+                     .groupby('year').agg('mean').round(decimals=2))
+            years.reset_index(inplace=True)
+
+            y = years.loc[(years['year'] >= 1996) & (years['year'] < 2021)]
+            y1 = y.reset_index(inplace=False).drop(columns='index')
+
+            x = years.loc[(years['year'] >= 1997) & (years['year'] < 2022)]
+            x1 = x.reset_index(inplace=False).drop(columns='index')
+
+            #merging y1 + x1 to calculate percentage differences
+            xy = pd.concat([x1, y1], axis=1, ignore_index=True)
+            xy.columns = ['year_1', 'index1', 'year_2', 'index2']
+
+            #building percentage_change_by_year with counted values
+            for ind, row in xy.iterrows():
+                xy.loc[ind, "percentage_change_by_previous_year"] = (row['index2'] - row['index1']) / row['index2'] * 100
+                table = xy.drop(columns=['index1', 'year_2', 'index2']).rename(columns={'year_1':  'year'})
+
+            #merging df with percentage_change_by_previous_year column
+            final_table = pd.merge(df, table, on='year')
+
+            return final_table
 
         else:
             print('Wrong table name or year 🙀')
@@ -66,9 +90,9 @@ class MeanFrames:
 
 
 # check for frames_total method
-x = MeanFrames()
-mean = x.frames_total('consumer_index', 1998, 2021)
-print(mean)
+# x = MeanFrames()
+# mean = x.frames_total('consumer_index', 1998, 2021)
+# print(mean)
 
 # check for single_frame method
 # x = MeanFrames()
